d6bc0205d2e77172cf5bb5480f67bc36c9644a1e
lubeys@msn.com <scott@lubey.com>
2020-03-27 15:27:20 -0700

    Updates

---
+++ precache-manifest.ce073c5fa5b00b2ab66cd09108a70601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f562291b35ef87500a95957cc8b328",
+    "url": "/reactcrud/index.html"
+  },
+  {
+    "revision": "87ab9e28deebb75374a2",
+    "url": "/reactcrud/static/css/main.5be9e5e2.chunk.css"
+  },
+  {
+    "revision": "969df689a8c6d07db67a",
+    "url": "/reactcrud/static/js/2.f3989312.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/reactcrud/static/js/2.f3989312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ab9e28deebb75374a2",
+    "url": "/reactcrud/static/js/main.a2dd38b7.chunk.js"
+  },
+  {
+    "revision": "466e77c3378623c6f8b8",
+    "url": "/reactcrud/static/js/runtime-main.b3f82bcc.js"
+  }
+]);
